18c4a696bf4cc28e9777ded1aaae0c2f91c2a253
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-13 14:01:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value });
 }
 
-console.log(object);
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return object = key[value];
+}
